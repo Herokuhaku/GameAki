deed8f1d1756b168f2bbe6ad33881141926bd8ac
herokuhaku <herokuhaku@gmail.com>
2021-04-27 11:16:57 +0900

    a

--- 新しいフォルダー/Project1/main.cpp
+++ 新しいフォルダー/Project1/main.cpp
@@ -1,8 +1,8 @@
-#include <DxLib.h>	// 自分で作った→"" ;  
+#include <DxLib.h>	// 自分で作ったもの→"" ;  
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd){
     ChangeWindowMode(TRUE);// Windowモードで起動
-    SetMainWindowText("俺のゲームだ！");//ウィンドウタイトルを変更
+    SetMainWindowText("MyGame");//ウィンドウタイトルを変更
     SetGraphMode(640, 480, 16);// 画面解像度と色数の設定
 
     if (DxLib_Init() == -1) {
@@ -12,9 +12,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 
     while (!ProcessMessage() && !CheckHitKey(KEY_INPUT_ESCAPE)) {
         ClsDrawScreen();
-
-
-
+        
         ScreenFlip();
     }
 
