a412f75908e25d9f750d8a7a5439b97c05ba8e8c
Herokuhaku <63630355+Herokuhaku@users.noreply.github.com>
2021-05-20 09:59:20 +0900

    バーに名前を入れた

--- 新しいフォルダー/Project1/main.cpp
+++ 新しいフォルダー/Project1/main.cpp
@@ -57,7 +57,7 @@ void MoveByInput(Position2 & pos ,const KeyBoardData& keystate)
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd){
     ChangeWindowMode(TRUE);// Windowモードで起動
-    SetMainWindowText(TEXT("MyGame"));//ウィンドウタイトルを変更
+    SetMainWindowText(TEXT("1916213_佐藤弘哲"));//ウィンドウタイトルを変更
     SetGraphMode(640, 480, 16);// 画面解像度と色数の設定
 
     if (DxLib_Init() == -1) {
