7c77b83399f281aa2199a7280ba014354f950026
Herokuhaku <63630355+Herokuhaku@users.noreply.github.com>
2021-06-15 11:17:17 +0900

    ぐにゃぐにゃ

---
+++ 新しいフォルダー/Project1/Geometry.cpp
@@ -0,0 +1,224 @@
+#include"Geometry.h"
+#include<DxLib.h>
+#include<cmath>
+#include <algorithm>
+
+void
+Rect::Draw() {
+	DxLib::DrawBox(Left()*2, Top()*2, Right()*2, Bottom()*2, 0xffffffff, false);
+}
+
+void
+Rect::Draw(Vector2& offset) {
+	DxLib::DrawBox((Left()+offset.x)*2, (Top()+offset.y)*2, (Right()+offset.x)*2, (Bottom()+offset.y)*2, 0xffffffff, false);
+}
+
+void
+Vector2::operator*=(float scale) {
+	x *= scale;
+	y *= scale;
+}
+
+void Vector2::operator/=(float div)
+{
+	x /= div;
+	y /= div;
+}
+
+bool Vector2::operator==(const Vector2& val)const
+{
+	return ((x == val.x) && (y == val.y));
+}
+
+void Vector2::Rotate90()
+{
+	std::swap(x,y);
+	x = -x;
+}
+
+Vector2
+Vector2::operator*(float scale) {
+	return Vector2(x*scale, y*scale);
+}
+
+Vector2 operator+(const Vector2& va, const Vector2 vb) {
+	return Vector2(va.x + vb.x, va.y + vb.y);
+}
+
+Vector2 operator-(const Vector2& va, const Vector2 vb){
+	return Vector2(va.x - vb.x, va.y - vb.y);
+}
+
+Vector2 operator*(const Vector2& va, float scale)
+{
+	return Vector2(va.x * scale,va.y * scale);
+}
+
+Vector2 operator/(const Vector2& va, float div)
+{
+	return Vector2(va.x / div, va.y / div);
+}
+
+float
+Vector2::Magnitude()const {
+	return hypot(x, y);
+}
+
+float Vector2::SQMagnitude() const
+{
+	return x*x + y*y;
+}
+
+
+void 
+Vector2::Normalize() {
+	float mag = Magnitude();
+	x /= mag;
+	y /= mag;
+}
+
+
+Vector2
+Vector2::Normalized()const {
+	Vector2 ret = {x,y};
+	ret.Normalize();
+	return ret;
+}
+
+
+///内積を返す
+float
+Dot(const Vector2& va, const Vector2& vb) {
+	return va.x*vb.x + va.y*vb.y;
+}
+
+///外積を返す
+float
+Cross(const Vector2& va, const Vector2& vb) {
+	return va.x*vb.y - vb.x*va.y;
+}
+
+///内積演算子
+float 
+operator*(const Vector2& va, const Vector2& vb) {
+	return Dot(va, vb);
+}
+
+///外積演算子
+float 
+operator%(const Vector2& va, const Vector2& vb) {
+	return Cross(va, vb);
+}
+
+
+void 
+Vector2::operator+=(const Vector2& v) {
+	x += v.x;
+	y += v.y;
+}
+void 
+Vector2::operator-=(const Vector2& v) {
+	x -= v.x;
+	y -= v.y;
+}
+
+
+//-------ここから3Dのターン------
+void
+Vector3::operator*=(float scale) {
+	x *= scale;
+	y *= scale;
+	z *= scale;
+}
+
+//Vector3
+//Vector3::operator*(const Vector3& v)const
+//{
+//	return Vector3(x * v.x, y * v.y, z * v.z);
+//}
+
+Vector3
+Vector3::operator*(float scale)const {
+	return Vector3(x*scale, y*scale,z*scale);
+}
+
+Vector3 operator+(const Vector3& va, const Vector3 vb) {
+	return Vector3(va.x + vb.x, va.y + vb.y,va.z+vb.z);
+}
+
+Vector3 operator-(const Vector3& va, const Vector3 vb) {
+	return Vector3(va.x - vb.x, va.y - vb.y,va.z-vb.z);
+}
+
+Vector3 operator*(float f, const Vector3 vb)
+{
+	return Vector3(f* vb.x,f * vb.y,f * vb.z);
+}
+
+float
+Vector3::Magnitude()const {
+	return sqrt(x*x+y*y+z*z);
+}
+
+
+void
+Vector3::Normalize() {
+	float mag = Magnitude();
+	x /= mag;
+	y /= mag;
+	z /= mag;
+}
+
+
+Vector3
+Vector3::Normalized() const{
+	float mag = Magnitude();
+	return Vector3(x / mag, y / mag,z/mag);
+}
+
+
+///内積を返す
+float
+Dot(const Vector3& va, const Vector3& vb){
+	return va.x*vb.x + va.y*vb.y+va.z*vb.z;
+}
+
+///外積を返す
+Vector3
+Cross(const Vector3& va, const Vector3& vb) {
+	return Vector3(va.z*vb.y-va.y*vb.z,va.z*vb.x-va.x*vb.z,va.x*vb.y - vb.x*va.y);
+}
+
+///内積演算子
+float
+operator*(const Vector3& va, const Vector3& vb) {
+	return Dot(va, vb);
+}
+
+///外積演算子
+Vector3
+operator%(const Vector3& va, const Vector3& vb) {
+	return Cross(va, vb);
+}
+
+Vector3 Max(const Vector3& v, const Vector3 b)
+{
+	Vector3 d;
+	d.x = max(v.x, b.x);
+	d.y = max(v.y, b.y);
+	d.z = max(v.z, b.z);
+	return d;
+}
+
+void
+Vector3::operator+=(const Vector3& v) {
+	x += v.x;
+	y += v.y;
+	z += v.z;
+}
+void
+Vector3::operator-=(const Vector3& v) {
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+}
--- 新しいフォルダー/Project1/main.cpp
+++ 新しいフォルダー/Project1/main.cpp
@@ -3,37 +3,34 @@
 #include <cassert>
 #include "_Debug.h"
 #include "Singleton.h"
+#include "Geometry.h"
 
-struct Vector2 {
-    float x, y;
-};
-
-using Position2 = Vector2;
 using KeyBoardData = std::array<char, 256>;
-struct Rect{
-    Position2 center;
-    float w;    // weight /2
-    float h;    // height / w
 
-    float Left()const {
-        return center.x - w;
-    }
-    float Top()const {
-        return center.y - h;
-    }
-    float Right()const {
-        return center.x + w;
-    }
-    float Bottom()const{
-        return center.y + h;
-    }
-    float Width()const {
-        return w * 2;
-    }
-    float Height()const{
-        return h * 2;
-    }
-};
+//struct Rect{
+//    Position2 center;
+//    float w;    // weight /2
+//    float h;    // height / w
+//
+//    float Left()const {
+//        return center.x - w;
+//    }
+//    float Top()const {
+//        return center.y - h;
+//    }
+//    float Right()const {
+//        return center.x + w;
+//    }
+//    float Bottom()const{
+//        return center.y + h;
+//    }
+//    float Width()const {
+//        return w * 2;
+//    }
+//    float Height()const{
+//        return h * 2;
+//    }
+//};
 
 void MoveByInput(Position2 & pos ,const KeyBoardData& keystate)
 {
@@ -56,6 +53,17 @@ void MoveByInput(Position2 & pos ,const KeyBoardData& keystate)
     }
 }
 
+Position2 GetMousePosition2() {
+    int mx, my;
+    GetMousePoint(&mx,&my);
+    return { static_cast<float>(mx),static_cast<float>(my) };
+}
+
+// 度数法をラジアンに変換
+float DegreeToRadian(float deg) {
+    return deg * DX_PI / 180.0f;
+}
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd){
     ChangeWindowMode(TRUE);// Windowモードで起動
     SetMainWindowText(TEXT("1916213_佐藤弘哲"));//ウィンドウタイトルを変更
@@ -71,25 +79,74 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
     int image_[6] = {};
     Rect rcA = {100,100,50,50};
     KeyBoardData keystate;
-    for (int i = 0; i < 6; i++) {
+    constexpr size_t picture_num = 5;
+    for (int i = 0; i < picture_num; i++) {
         wchar_t path[80];
-        wsprintf(path,L"Asset/Adventurer-1.5/Individual Sprites/adventurer-run-%02d.png", i);
+        //wsprintf(path,L"Asset/Adventurer-1.5/Individual Sprites/adventurer-run-%02d.png", i);
+        wsprintf(path, L"Asset/Adventurer-1.5/Individual Sprites/adventurer-attack1-%02d.png", i);
         image_[i] = LoadGraph(path);
         assert(image_[i] != -1);
     }
-
+    int groundH = LoadGraph(L"Asset/ground.png");
+    int bgAssetH = LoadGraph(L"Asset/Assets.png");
     unsigned int frameNo = 0;
+    constexpr size_t frames_per_pict = 5;
+    int lastMouseInput = 0;
+    bool isReverse = false; 
+    int frame_for_angle = 0;
     while (!ProcessMessage() && !CheckHitKey(KEY_INPUT_ESCAPE)) {
         ClsDrawScreen();
 
         GetHitKeyStateAll(keystate.data());
         
+        auto currentMouseInput = GetMouseInput();
+        if ((currentMouseInput & MOUSE_INPUT_LEFT) &&
+            !(lastMouseInput & MOUSE_INPUT_LEFT)) {
+            isReverse = !isReverse;
+        }
+        lastMouseInput = currentMouseInput;
+
         MoveByInput(rcA.center,keystate);
 
         DrawBox(rcA.Left(),rcA.Top(),rcA.Right(),rcA.Bottom(),0xffffff,true);
        
+        // 現在のマウス座標を取得
+        auto mpos = GetMousePosition2();
+        Vector2 dir = mpos - rcA.center;
+        // 基準(0°)が右ベクトルで、マウスの向きを角度で返す
+        float angle = atan2(dir.y, dir.x);
 
-        DrawRotaGraph(rcA.center.x,rcA.center.y,3.0f,0.0f,image_[frameNo/10],true);
+        auto imgIdx = frameNo / frames_per_pict;
+        int centerx = 16;
+        if (isReverse) {
+            int w, h;
+            GetGraphSize(image_[imgIdx],&w,&h);
+            centerx = w - centerx;
+        }
+
+
+        // 地面の描画
+        constexpr int block_size = 32;
+        float base_y = 240;
+        auto count = 720 / block_size;
+        float x = 0;
+        float y = base_y;
+        for (int i = 0; i < count; ++i) {
+            float nextx = i * block_size;
+            float ang = DegreeToRadian(nextx * 0.5 + frame_for_angle);
+            float nexty = sin(ang) * 100 + base_y;
+            DrawLineAA(x,y,nextx,nexty,0xffffff,5.0f);
+            //DrawModiGraph(x,y,nextx,nexty,nextx,nexty+block_size,x,y+block_size,groundH,true);
+            DrawRectModiGraph(x, y, nextx, nexty, nextx, nexty + block_size, x, y + block_size,48,0,16,16,bgAssetH, true);
+
+            x = nextx;
+            y = nexty;
+        }
+
+        DrawRotaGraph2(rcA.center.x, rcA.center.y,centerx,35, 4.0f,0.0f, image_[frameNo / frames_per_pict], true,isReverse);
+
+        //DrawRotaGraph(rcA.center.x,rcA.center.y,3.0f,angle,image_[frameNo/frames_per_pict],true);
+        DrawCircleAA(rcA.center.x, rcA.center.y, 5.0f, 16, 0x00ff00);
         //Debug::OutLine(16);
         //Debug::OutLine(static_cast<float>(16.555));
         //Debug::OutLine("Debug Out");
@@ -97,11 +154,11 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
         Singleton& single = Singleton::GetInstance();
         single.OutputLife();
 
-
         ScreenFlip();
-        frameNo = (frameNo + 1) % 60;
+        frameNo = (frameNo + 1) % (frames_per_pict*picture_num);
+        frame_for_angle = (frame_for_angle + 1) % 720;
     }
 
-    DxLib_End();// ＤＸライブラリ終了
+    DxLib::DxLib_End();// ＤＸライブラリ終了
     return 1;
 }
